633c051def3558d330f42ebcac6d616ac151c796
Joey <30815220+joeylking@users.noreply.github.com>
2021-10-20 14:17:17 -0500

    Tests for user_interface.get_unique_can_names function

--- test_user_interface.py
+++ test_user_interface.py
@@ -1,5 +1,6 @@
 import unittest
 import user_interface
+import cans
 
 class TestValidateMainMenu(unittest.TestCase):
     """Tests for the validate_main_menu function in user_interface module"""
@@ -34,6 +35,26 @@ class TestTryParseInt(unittest.TestCase):
     def test_invalid_string_returns_0(self):
         output = user_interface.try_parse_int("hello")
         self.assertEqual(output, 0)
+
+class TestGetUniqueCanNames(unittest.TestCase):
+    """Tests for the get_unique_can_names function in user_interface module"""
+    
+    def test_input_with_duplicate_cans_returns_list_of_unique_names(self):
+        can1 = cans.Cola()
+        can2 = cans.Cola()
+        can3 = cans.OrangeSoda()
+        can4 = cans.OrangeSoda()
+        can5 = cans.RootBeer()
+        can6 = cans.RootBeer()
+        can_list = [can1, can2, can3, can4, can5, can6]
+        output = user_interface.get_unique_can_names(can_list)
+        self.assertEqual(3, len(output))
+
+    def test_input_empty_list_returns_empty_list(self):
+        can_list = []
+        output = user_interface.get_unique_can_names(can_list)
+        self.assertEqual(0, len(output))
+
 
 if __name__ == '__main__':
     unittest.main()
