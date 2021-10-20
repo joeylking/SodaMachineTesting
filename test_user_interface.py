09a74772f83ffc0454258a9fb090f8f03e015dcb
Joey <30815220+joeylking@users.noreply.github.com>
2021-10-20 14:05:13 -0500

    Tests for user_interface.try_parse_int function. Test for validate_main_menu invalid input

--- test_user_interface.py
+++ test_user_interface.py
@@ -20,5 +20,20 @@ class TestValidateMainMenu(unittest.TestCase):
         output = user_interface.validate_main_menu(4)
         self.assertEqual(output, (True, 4))
 
+    def test_invalid_input_returns_proper_tuple(self):
+        output = user_interface.validate_main_menu(5)
+        self.assertEqual(output, (False, None))
+
+class TestTryParseInt(unittest.TestCase):
+    """Tests for the try_parse_int function in user_interface module"""
+
+    def test_number_string_returns_int(self):
+        output = user_interface.try_parse_int("10")
+        self.assertEqual(output, 10)
+
+    def test_invalid_string_returns_0(self):
+        output = user_interface.try_parse_int("hello")
+        self.assertEqual(output, 0)
+
 if __name__ == '__main__':
     unittest.main()
